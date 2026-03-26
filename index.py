41320bd8dce443345e1fc1fbc484c38e85d8e619
Joanna <lifangcheng2005@gmail.com>
2026-03-26 13:01:23 +0800

    v1

--- index.py
+++ index.py
@@ -5,16 +5,17 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     link = "<h1>歡迎進入程莉芳的網站</h1>"
-    link += "<a href=/mis>課程</a><hr>"
-    link += "<a href=/today>現在日期時間</a><hr>"
-    link += "<a href=/me>關於我</a><hr>"
-    link += "<a href=/welcome?u=莉芳&d=靜宜資管>Get傳值</a><hr>"
-    link += "<a href=/account>傳值</a><br>"
+    link += "<a href='/mis'>課程</a><hr>"
+    link += "<a href='/today'>現在日期時間</a><hr>"
+    link += "<a href='/me'>關於我</a><hr>"
+    link += "<a href='/welcome?u=莉芳&d=靜宜資管'>Get傳值</a><hr>"
+    link += "<a href='/account'>POST傳值</a><br>"
+    link += "<a href='/math'>次方與根號</a><br>"
     return link
 
 @app.route("/mis")
 def course():
-    return "<h1>資訊管理導論</h1><a href=/>返回首頁</a>"
+    return "<h1>資訊管理導論</h1><a href='/'>返回首頁</a>"
 
 @app.route("/today")
 def today():
@@ -27,7 +28,8 @@ def me():
 
 @app.route("/welcome", methods=["GET"])
 def welcome():
-    user = request.values.get("nick")
+    # 修正：參數名稱從 nick 改為 u
+    user = request.values.get("u")
     return render_template("welcome.html", name=user)
 
 @app.route("/account", methods=["GET", "POST"])
@@ -40,6 +42,31 @@ def account():
     else:
         return render_template("account.html")
 
+@app.route("/math", methods=["GET", "POST"])
+def math():
+    if request.method == "POST":
+        try:
+            x = int(request.form["x"])
+            y = int(request.form["y"])
+            opt = request.form["opt"]
+
+            if opt == "^":
+                result = x ** y
+            elif opt == "√":  # 修正：刪除後方的字母 a
+                if y == 0:
+                    result = "數學不能開0次方根"
+                else:
+                    result = x ** (1/y)
+            else:
+                result = "請輸入^或√"
+
+            # 修正：調整 return 的縮排，確保計算完畢後回傳結果
+            return f"計算結果: {result}<br><br><a href='/math'>繼續計算</a> | <a href='/'>回首頁</a>"
+
+        except ValueError:
+            return "輸入格式錯誤，請確保 x 和 y 都是整數！<br><a href='/math'>返回重新計算</a>"
+    else:
+        return render_template("math.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
